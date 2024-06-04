307517401da045158b43ca2852efc176fbad8650
kams <1817kams@gmail.com>
2024-06-04 14:14:55 +0000

    upload all files first time

---
+++ src/components/Cart/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+// Write your JS code here
+import './index.css'
+
+const Cart = () => {
+  return (
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+    </div>
+  )
+}
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const Products = () => {
+  return (
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+    </div>
+  )
+}
+
+export default Products
